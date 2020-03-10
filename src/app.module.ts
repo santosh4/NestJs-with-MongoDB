96352db8a084ad3d176108ceb9e2507d79f9e4ce
Santosh Mutyala <musantosh@deloitte.com>
2020-03-11 01:19:00 +0530

    Second commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://santosh:gmbntrnbk@cluster0-rvss8.mongodb.net/test?retryWrites=true&w=majority')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
